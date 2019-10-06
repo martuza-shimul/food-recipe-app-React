b0902da161f3c98350bc312a3c8911840a9e39e7
Martuza Shimul <martuza.shimul@gmail.com>
2019-10-06 17:12:55 +0600

    final version 1.0

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React, {useEffect, useState} from 'react';
-import Recipe from './Recipe'
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Recipe from "./Recipe";
+import "./App.css";
 
 const App = () => {
-
   const APP_ID = "145c442f";
   const APP_KEY = "fa595d2525ce712a15ee6856ae21d94c";
-  
+
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState("");
-  const [query, setQuery] = useState("chicken")
+  const [query, setQuery] = useState("chicken");
 
   useEffect(() => {
     // console.log('Effect has been run');
@@ -17,7 +16,9 @@ const App = () => {
   }, [query]);
 
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
     const data = await response.json();
     setRecipes(data.hits);
     console.log(data.hits);
@@ -31,25 +32,28 @@ const App = () => {
   const getSearch = e => {
     e.preventDefault();
     setQuery(search);
-    setSearch('');
-  }
+    setSearch("");
+  };
 
-  
-
-  return(
+  return (
     <div className="App">
       <form onSubmit={getSearch} className="search-form">
-        <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
-        <button  className="search-button" type="submit">
+        <input
+          className="search-bar"
+          type="text"
+          value={search}
+          onChange={updateSearch}
+        />
+        <button className="search-button" type="submit">
           Search
         </button>
       </form>
       <div className="recipes">
         {recipes.map(recipe => (
-          <Recipe 
-            key={recipe.recipe.label} 
-            title={recipe.recipe.label} 
-            calories={recipe.recipe.calories} 
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
             image={recipe.recipe.image}
             ingredients={recipe.recipe.ingredients}
           />
@@ -57,6 +61,6 @@ const App = () => {
       </div>
     </div>
   );
-}
+};
 
 export default App;
